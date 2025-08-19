e2c304f5cf6abcbb95128798ce272c775c373be4
Neel Dandiwala <neeldandi@gmail.com>
2025-08-20 01:27:42 +0530

    github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // IMPORTANT: use your repo name here (keep the trailing slash)
+  base: '/zeal/',
   build: {
     rollupOptions: {
       output: {
